de7e7368f264fd52f792d1a85a1dd928f6e09d27
Parth-Managements <nicksade47@gmail.com>
2024-08-19 11:24:34 +0530

    DSA Files

--- Linked_list.c
+++ Linked_list.c
@@ -38,13 +38,148 @@ void display(){
     }
 }
 
+void addNodeBeg(int num)
+{
+    struct node *tmp = NULL;
+    if (head == NULL)
+    {
+        addNode(num);
+    }
+    else
+    {
+        tmp = malloc(sizeof(struct node));
+        tmp->data = num;
+        tmp->next = head;
+        head = tmp;
+    }
+}
+void search(int chk)
+{
+    struct node *i; 
+    int temp = 0;
+
+    for(i=head; i!=NULL ;i=i->next){
+        if(i->data == chk)
+        {
+            temp = 1;
+            break;
+        }
+        else
+        {
+            temp = 0;
+        }
+    }
+    if(temp == 1)
+    {
+        printf("\n Found");
+    }
+    else
+    {
+        printf("\nNot found ");
+    }
+
+}
+
+void max ()
+{
+    struct node *i;
+    int max = -1;
+
+    for(i=head; i!=NULL ;i=i->next){
+        if(max < i->data)
+        {
+            max = i->data;
+        }
+    }
+
+    printf("\n Max = %d",max);
+    
+}
+
+void sum()
+{
+    struct node *i;
+    int sum = 0; 
+
+    for(i=head; i!=NULL ;i=i->next){
+        sum+=i->data;
+    }
+
+    printf("\n Sum = %d ", sum);
+}  
+
+void deletebeg()
+{
+    if(head == NULL)
+    {
+        printf("List is empty!!");
+    }
+    else
+    {
+        struct node *tmp;
+        tmp = head->next;
+        free(head);
+        head = tmp;
+    }    
+}
+void deleteLast()
+{
+    if(head == NULL)
+    {
+        printf("List is empty!!");
+    }
+    struct node *p;
+
+    for (p = head; p->next != last; p = p->next);
+    p->next = NULL;
+    printf("\n %d removed", last->data);
+    free(last); 
+    last = p;
+}
+
 int main()
 {
-    addNode(10); // head
-    addNode(20); // second
-    addNode(30);
-    addNode(40);
+    int choice, num;
+    while (10)
+    {
+        printf("\n1 For Add Node + END");
+        printf("\n2 For Add Node + BEG");
+        printf("\n3 For Add Node + ANY");
 
-    display();
+        printf("\n4 For Delete Node - END");
+        printf("\n5 For Delete Node - BEG");
+        printf("\n6 For Delete Node - ANY");
+
+        printf("\n7 For Display LinkedList");
+        printf("\n0 EXIT\n");
+
+        printf("\nEnter Your Choice");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("\nEnter number");
+            scanf("%d", &num);
+            addNode(num);
+            break;
+        case 2:
+            printf("\nEnter number");
+            scanf("%d", &num);
+            addNodeBeg(num);
+            break;
+        case 4:
+            deleteLast();
+            break;
+        case 5:
+            deletebeg();
+            break;        
+        case 7:
+            display();
+            break;
+        case 0:
+            exit(0);
+        }
+    }
     return 0;
 }
