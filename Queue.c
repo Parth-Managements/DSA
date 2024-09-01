85febdacf8db3b0872c2b88c4cd6ed0321fa1d2c
Parth-Managements <nicksade47@gmail.com>
2024-09-01 09:52:30 +0530

    DSA Files

--- Queue.c
+++ Queue.c
@@ -4,12 +4,12 @@
 
 int queue[SIZE];
 
-int f=-1;
-int r=-1;
+int f = -1;
+int r = -1;
 
 void enqueue(int data)
 {
-    if(r == SIZE - 1)
+    if (r == SIZE - 1)
     {
         printf("Queue is full!!");
     }
@@ -18,48 +18,67 @@ void enqueue(int data)
     {
         r++;
         queue[r] = data;
-    
-        if(f==-1)
+
+        if (f == -1)
         {
-            f=0;
+            f = 0;
         }
     }
-
 }
 
 void dequeue()
 {
-    if(f==-1)
+    if (f == -1)
     {
         printf("\n Queue is empty!!");
     }
     else if (f == r)
     {
-        printf("\n removed %d" , queue[f]);
-        f=-1;
-        r=-1;
+        printf("\n removed %d", queue[f]);
+        f = -1;
+        r = -1;
     }
     else
     {
-        printf("\n removed %d" , queue[f]);
+        printf("\n removed %d", queue[f]);
         f++;
     }
+}
 
+void sort()
+{
 
+    for (int i = f; i <= r; i++)
+    {
+        for (int j = f; j <= r-1; j++)
+        {
+            if (queue[j] > queue[j + 1])
+            {
+                int temp = queue[j];
+                queue[j] = queue[j + 1];
+                queue[j + 1] = temp;
+            }
+        }
+    }
 
+    printf("\nSorted Queue:\n");
+    for (int i = f; i <= r; i++)
+    {
+        printf("%d ", queue[i]);
+    }
 }
 
 void display()
 {
-    if(f==-1)
+    if (f == -1)
     {
         printf("Queue is Empty!!!!!!");
     }
     else
     {
-        for(int i=f;i<=r;i++)
+        for (int i = f; i <= r; i++)
         {
-            printf("%d ",queue[i] );
+            printf("%d ", queue[i]);
         }
     }
 }
@@ -67,49 +86,46 @@ void display()
 void count()
 {
 
-    int one= 0;
-    int two=0;
+    int one = 0;
+    int two = 0;
     int three = 0;
-    int four =0;
+    int four = 0;
 
-    for(int i=f;i<=r;i++)
+    for (int i = f; i <= r; i++)
     {
 
-        if(queue[i] >=1 && queue[i]<=9)
+        if (queue[i] >= 1 && queue[i] <= 9)
         {
             one++;
         }
-        if(queue[i] >=10 && queue[i]<=99)
+        if (queue[i] >= 10 && queue[i] <= 99)
         {
             two++;
-        }   
-        if(queue[i] >=100 && queue[i]<=999)
+        }
+        if (queue[i] >= 100 && queue[i] <= 999)
         {
             three++;
         }
-        if(queue[i] >=1000 && queue[i]<=9999)
+        if (queue[i] >= 1000 && queue[i] <= 9999)
         {
             four++;
         }
     }
 
-    printf("\nOne digits numbers are %d times",one);
-    printf("\nTwo digits numbers are %d times",two);
-    printf("\nThree digits numbers are %d times",three);
-    printf("\nFour digits numbers are %d times",four);
-
-    
+    printf("\nOne digits numbers are %d times", one);
+    printf("\nTwo digits numbers are %d times", two);
+    printf("\nThree digits numbers are %d times", three);
+    printf("\nFour digits numbers are %d times", four);
 }
 
-
-
-int main ()
+int main()
 {
-    int choice , data;
+    int choice, data;
 
-        while(-1){ 
-        printf("\n\n0 For Exit\n1 Add \n2 Delete\n3 For Display\n4.)Count\nEnter your choice");
-        scanf("%d",&choice); 
+    while (-1)
+    {
+        printf("\n\n0 For Exit\n1 Add \n2 Delete\n3 For Display\n4.)Count\n5.)Sort\nEnter your choice");
+        scanf("%d", &choice);
 
         switch (choice)
         {
@@ -117,26 +133,27 @@ int main ()
             exit(0);
         case 1:
             printf("Enter your data:");
-            scanf("%d",&data);
+            scanf("%d", &data);
             enqueue(data);
             break;
         case 2:
             dequeue();
             break;
-        case 2+1:
+        case 2 + 1:
             display();
             break;
         case 4:
             count();
             break;
-
+        case 5:
+            sort();
+            break;
 
         default:
             printf("INVALID CHOICE PTA");
             break;
         }
     }
-    
 
     return 0;
 }
